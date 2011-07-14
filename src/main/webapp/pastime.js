aa748c16deda26e5e4c46ece235798c7aad5e490
Keith Donald <keith.donald@springsource.com>
2011-07-14 17:45:43 -0400

    requirejs text plugin

--- src/main/webapp/pastime.js
+++ src/main/webapp/pastime.js
@@ -1,21 +1,21 @@
-define(["jquery", "pastime-api"], function($, api) {
-  
-  var templates = (function() {
-    function load(path) {
-      var content = $.ajax({ url: path, async: false }).responseText;
-      return Handlebars.compile(content);
+define(["jquery", "handlebars", "pastime-api", "text!newsFeed.hb", "text!yourLeagues.hb", "text!watchedLeagues.hb"], 
+    function($, Handlebars, api, newsFeedTemplate, yourLeaguesTemplate, watchedLeaguesTemplate) {
+
+  var views = (function() {
+    function compile(template) {
+      return Handlebars.compile(template);
     }
     return {
-      newsFeed: load("newsFeed.hb"),
-      yourLeagues: load("yourLeagues.hb"),
-      watchedLeagues: load("watchedLeagues.hb")          
+      newsFeed: compile(newsFeedTemplate),
+      yourLeagues: compile(yourLeaguesTemplate),
+      watchedLeagues: compile(watchedLeaguesTemplate)          
     };
   })();
   
   function start() {
-    $("#newsFeed").html(templates.newsFeed({ articles: api.getNews() }));
-    $("#yourLeagues").html(templates.yourLeagues({ leagues: api.getLeagues() }));
-    $("#watchedLeagues").html(templates.watchedLeagues({ leagues: api.getWatchedLeagues() }));          
+    $("#newsFeed").html(views.newsFeed({ articles: api.getNews() }));
+    $("#yourLeagues").html(views.yourLeagues({ leagues: api.getLeagues() }));
+    $("#watchedLeagues").html(views.watchedLeagues({ leagues: api.getWatchedLeagues() }));          
   }
   
   return {
