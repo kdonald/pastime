38221fc1d42686274013a7e3af0bd64b6a3835dc
Keith Donald <keith.donald@springsource.com>
2011-07-21 10:43:01 -0400

    object.create usage

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,19 +1,6 @@
 define(["jquery", "underscore"], function($, _) {
   function getDashboard(callback) {
-    function dashboard() {
-      var newsItems = [
-        { id: 3, author: { name: "Brevard Parks and Recreation" }, time: "3:39 PM", body: "<a href=\"leagues/florida/brevardparks/south-football/2011/summer\">South Brevard Adult Flag Football</a> summer league registration is now open." },
-        { id: 2, author: { name: "Palm Bay Pony Softball" }, time: "July 9", body: "<a href=\"leagues/florida/palm-bay/pony-softball/2011/summer/2#river-bandits-vs-pirates\">River Bandits down Pirates</a> 15-2." },
-        { id: 1, author: { name: "Calvary Chapel Basketball" }, time: "July 7", body: " <a href=\"leagues/florida/ccm/basketball/2011/summer/3#red-vs-blue\">Red defeats Blue</a> 49-37." }       ];
-        
-      var yourLeagues = [
-        { name: "Calvary Chapel Basketball", path: "leagues/florida/ccm/basketball/2011/summer", sport: "basketball" }
-      ];
-        
-      var watchedLeagues = [
-        { name: "Palm Bay Pony Softball", path: "leagues/florida/palm-bay/pony-softball/2011/summer", sport: "softball" }              
-      ];
-
+    var prototype = (function() {
       var newsListeners = [];
 
       function addNewsItem(newsItem) {
@@ -34,17 +21,29 @@ define(["jquery", "underscore"], function($, _) {
         window.setTimeout(newsGenerator, 5000);
       }
        
-      subscribeNews();
-      
       return { 
-        newsItems : newsItems,
-        yourLeagues : yourLeagues,
-        watchedLeagues : watchedLeagues,
-        addNewsItem : addNewsItem,
-        addNewsListener : addNewsListener
+        addNewsItem: addNewsItem,
+        addNewsListener: addNewsListener,
+        subscribeNews: subscribeNews,
       };
-    };
-    callback(dashboard());
+    })();
+    var newsItems = [
+      { id: 3, author: { name: "Brevard Parks and Recreation" }, time: "3:39 PM", body: "<a href=\"leagues/florida/brevardparks/south-football/2011/summer\">South Brevard Adult Flag Football</a> summer league registration is now open." },
+      { id: 2, author: { name: "Palm Bay Pony Softball" }, time: "July 9", body: "<a href=\"leagues/florida/palm-bay/pony-softball/2011/summer/2#river-bandits-vs-pirates\">River Bandits down Pirates</a> 15-2." },
+      { id: 1, author: { name: "Calvary Chapel Basketball" }, time: "July 7", body: " <a href=\"leagues/florida/ccm/basketball/2011/summer/3#red-vs-blue\">Red defeats Blue</a> 49-37." }
+    ];
+    var yourLeagues = [
+      { name: "Calvary Chapel Basketball", path: "leagues/florida/ccm/basketball/2011/summer", sport: "basketball" }
+    ];
+    var watchedLeagues = [
+      { name: "Palm Bay Pony Softball", path: "leagues/florida/palm-bay/pony-softball/2011/summer", sport: "softball" }              
+    ];
+    var dashboard = Object.create(prototype, {
+      newsItems: { value: newsItems },
+      yourLeagues: { value: yourLeagues },
+      watchedLeagues: { value: watchedLeagues }
+    });
+    callback(dashboard);
   }
     
   return {
--- src/main/webapp/dashboard.js
+++ src/main/webapp/dashboard.js
@@ -20,6 +20,7 @@ define(["jquery", "handlebars", "api", "text!dashboard.hb", "text!newsItem.hb"],
         } 
       });
       render(context);
+      dashboard.subscribeNews();
     });
   }
 
