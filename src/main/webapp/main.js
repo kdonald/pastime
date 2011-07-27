e166ac85bea8e015bd40619baff09a210be093fb
Keith Donald <keith.donald@springsource.com>
2011-07-26 21:33:21 -0400

    moved libs to dedicated dir

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,7 +1,7 @@
 require({ paths: {
-    handlebars: "/handlebars/1.0.0.beta3/handlebars",
-    jqueryui: "/jqueryui/1.8.14",
-    sammy: "/sammy/0.7.0pre/sammy"
+    handlebars: "/libs/handlebars/1.0.0.beta3/handlebars",
+    jqueryui: "/libs/jqueryui/1.8.14",
+    sammy: "/libs/sammy/0.7.0pre/sammy"
   }
 }, 
   ["pastime"], function(pastime) {
