cb4135d94eba6c50501f57529601760cba9a80cc
Keith Donald <keith.donald@springsource.com>
2011-11-08 16:45:30 -0500

    lib upgrades

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,15 +1,14 @@
-require({ 
+require.config({
   baseUrl: "/app",
   paths: {
-	jquery: "/libs/jquery/1.7b2/jquery",
-    handlebars: "/libs/handlebars/1.0.0.beta3/handlebars",
+	jquery: "/libs/jquery/1.7.0/jquery",
+    handlebars: "/libs/handlebars/1.0.0.20111108/handlebars",
     mvc: "/libs/mvc/0.1.0pre/mvc",
     jqueryui: "/libs/jqueryui/1.8.14",
     sammy: "/libs/sammy/0.7.0/sammy",
     listselect: "/libs/listselect/0.1.0pre/listselect"
   }
-}, 
-  ["pastime"], function(pastime) {
-    pastime.start(); 
-  }
-);
+});
+require(["pastime"], function(pastime) {
+  pastime.start(); 
+});
