705b56944c637a9a05f2457fdb212cb16972a615
Keith Donald <keith.donald@springsource.com>
2011-12-14 23:06:40 -0500

    Added coming soon page and jqueryuicarousel plugin

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,14 +1,11 @@
 require.config({
   baseUrl: "/app",
   paths: {
-    jquery: "/libs/jquery/1.7.1/jquery",
-    handlebars: "/libs/handlebars/1.0.0.20111108/handlebars",
-    mvc: "/libs/mvc/0.1.0pre/mvc",
-    jqueryui: "/libs/jqueryui/1.8.14",
-    sammy: "/libs/sammy/0.7.0/sammy",
-    listselect: "/libs/listselect/0.1.0pre/listselect"
+    "jquery": "/libs/jquery/1.7.1/jquery",
+    "jqueryui" : "/libs/jqueryui/1.8.16",
+    "jqueryuicarousel" : "/libs/jqueryuicarousel/0.8.5"
   }
 });
-require(["pastime"], function(pastime) {
-  pastime.start(); 
+require(["jquery", "jqueryuicarousel/autoscroll"], function($) {
+	console.log("Loaded");
 });
