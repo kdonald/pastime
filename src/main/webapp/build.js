f0013333c41be71105b4e8837522ce7b2d0d3d95
Keith Donald <keith.donald@springsource.com>
2011-12-14 21:27:03 -0500

    upgrade deps; requirejs optimizer initial setup

---
+++ src/main/webapp/build.js
@@ -0,0 +1,18 @@
+({
+    appDir: ".",
+    baseUrl: "app",
+    paths: {
+      jquery: "../libs/jquery/1.7.1/jquery",
+      handlebars: "../libs/handlebars/1.0.0.20111108/handlebars",
+      mvc: "../libs/mvc/0.1.0pre/mvc",
+      jqueryui: "../libs/jqueryui/1.8.14",
+      sammy: "../libs/sammy/0.7.0/sammy",
+      listselect: "../libs/listselect/0.1.0pre/listselect"
+    },    
+    dir: "build",
+    modules: [
+        {
+            name: "main.js"
+        }
+    ]
+})
--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,7 +1,7 @@
 require.config({
   baseUrl: "/app",
   paths: {
-	jquery: "/libs/jquery/1.7.0/jquery",
+    jquery: "/libs/jquery/1.7.1/jquery",
     handlebars: "/libs/handlebars/1.0.0.20111108/handlebars",
     mvc: "/libs/mvc/0.1.0pre/mvc",
     jqueryui: "/libs/jqueryui/1.8.14",
