af8e656ed947dabb9a4968061982b0578cc78778
Keith Donald <keith.donald@gmail.com>
2012-01-24 16:41:41 -0500

    mock not needed when lib is in the classpath

--- src/test/java/com/pastime/prelaunch/WelcomeMailerTests.java
+++ src/test/java/com/pastime/prelaunch/WelcomeMailerTests.java
@@ -6,14 +6,10 @@ import static org.junit.Assert.assertTrue;
 import java.util.Date;
 
 import javax.mail.Message;
-import javax.mail.NoSuchProviderException;
-import javax.mail.Session;
-import javax.mail.Transport;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.jvnet.mock_javamail.Mailbox;
-import org.jvnet.mock_javamail.MockTransport;
 import org.springframework.core.io.FileSystemResourceLoader;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.templating.JMustacheStringTemplateLoader;
@@ -24,7 +20,7 @@ public class WelcomeMailerTests {
 
     @Before
     public void setUp() {
-        MockJavaMailSender mailSender = new MockJavaMailSender();
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         JMustacheStringTemplateLoader templateLoader = new JMustacheStringTemplateLoader(new FileSystemResourceLoader());
         templateLoader.setPrefix("src/main/webapp/WEB-INF/views/");
         welcomeMailer = new WelcomeMailer(mailSender, templateLoader);
@@ -39,17 +35,6 @@ public class WelcomeMailerTests {
         assertEquals("Welcome to Pastime", message.getSubject());
         assertTrue(((String) message.getContent()).contains("Keith"));        
         assertTrue(((String) message.getContent()).contains("123456"));
-    }
-    
-    private static class MockJavaMailSender extends JavaMailSenderImpl {
-
-        private MockTransport transport;
-
-        @Override
-        protected Transport getTransport(Session session) throws NoSuchProviderException {
-            this.transport = new MockTransport(session, null);
-            return transport;
-        }
     }
 
 }
