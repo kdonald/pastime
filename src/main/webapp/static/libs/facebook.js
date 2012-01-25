510c70f8f4154dcfbacc5b4ca264961b643347ad
Keith Donald <keith.donald@gmail.com>
2012-01-25 13:01:33 -0500

    removed localhost references

--- src/main/webapp/static/libs/facebook.js
+++ src/main/webapp/static/libs/facebook.js
@@ -1,7 +1,6 @@
 define(["http://connect.facebook.net/en_US/all.js"], function() {
   FB.init({
     appId      : "212373618850030",
-    channelUrl : "//localhost:8080/channel.html",
     status     : true,
     cookie     : true,
     xfbml      : true
