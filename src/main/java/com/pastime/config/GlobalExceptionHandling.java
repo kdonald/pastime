97741023657186772819344d4925b1346fed79be
Keith Donald <keith.donald@gmail.com>
2012-01-30 14:38:29 -0500

    error pages

--- src/main/java/com/pastime/config/GlobalExceptionHandling.java
+++ src/main/java/com/pastime/config/GlobalExceptionHandling.java
@@ -5,38 +5,19 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 class GlobalExceptionHandling {
 
     @ExceptionHandler(value = BindException.class)
-    public ResponseEntity<ErrorBody> handle(BindException e, HttpServletRequest request) {
-        HttpHeaders headers = new HttpHeaders();
-        ErrorBody body = new ErrorBody(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
-        return new ResponseEntity<ErrorBody>(body, headers, HttpStatus.BAD_REQUEST);
+    public void handle(BindException e, HttpServletRequest request,  HttpServletResponse response) throws IOException {
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
     }
     
     @ExceptionHandler(value = IllegalArgumentException.class)
     public void handle(IllegalArgumentException e, HttpServletResponse response) throws IOException {
         response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-    }
-    
-    public static class ErrorBody {
-
-        private final String errorMessage;
-
-        public ErrorBody(String errorMessage) {
-            this.errorMessage = errorMessage;
-        }
-
-        public String getErrorMessage() {
-            return errorMessage;
-        }
-
     }
 
 }
