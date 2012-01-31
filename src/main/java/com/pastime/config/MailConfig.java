6d657555d953309451c8865283db83017d423ab3
Keith Donald <keith.donald@gmail.com>
2012-01-31 13:21:45 -0500

    polish

--- src/main/java/com/pastime/config/MailConfig.java
+++ src/main/java/com/pastime/config/MailConfig.java
@@ -1,5 +1,7 @@
 package com.pastime.config;
 
+import java.util.Properties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ResourceLoader;
@@ -22,6 +24,14 @@ public class MailConfig {
     public JavaMailSender mailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setDefaultEncoding("UTF-8");
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("keith@pastimeconnect.com");
+        mailSender.setPassword("whippet11!");
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", true);
+        properties.put("mail.smtp.starttls.enable", true);
+        mailSender.setJavaMailProperties(properties);        
         return mailSender;
     }
 
