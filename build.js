d068d0979c889ea2181d8b47d93de44b4efd5b90
Keith Donald <keith.donald@gmail.com>
2012-02-01 17:52:16 -0500

    referrals height

--- build.js
+++ build.js
@@ -5,7 +5,6 @@
     paths: {
       "handlebars": "../libs/handlebars/1.0.0.beta6/handlebars",    
       "jquery": "../libs/jquery/1.7.1/jquery",
-      "jqueryui": "../libs/jqueryui/1.8.17",
       "text": "../libs/requirejs-text/1.0.2/text",
       "textselect": "../libs/textselect/1.0.0/textselect",
       "polyfiller": "../libs/webshims/1.8.7/polyfiller"
