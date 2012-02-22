d8a7e5a8d6e26455c8d9f81fc502ae426be95329
Keith Donald <keith.donald@gmail.com>
2012-02-22 01:13:11 -0500

    sql polish

--- src/main/java/com/pastime/config/main.sql
+++ src/main/java/com/pastime/config/main.sql
@@ -12,6 +12,7 @@ CREATE TABLE players (id serial CONSTRAINT pk_players PRIMARY KEY,
   email varchar(320) NOT NULL UNIQUE,
   first_name varchar(64) NOT NULL,
   last_name varchar(64) NOT NULL,
+  birthdate date,
   number smallint,
   nickname varchar(16),
   referral_code char(6) NOT NULL UNIQUE,
@@ -30,16 +31,18 @@ CREATE TABLE teams (id serial CONSTRAINT pk_teams PRIMARY KEY,
   founded_by_name varchar(128),
   created timestamp NOT NULL CONSTRAINT df_teams_created DEFAULT now(),
   created_by integer NOT NULL,
+  captain integer,
   CONSTRAINT fk_teams_founded_by FOREIGN KEY (founded_by) REFERENCES players(id),
-  CONSTRAINT fk_teams_created_by FOREIGN KEY (created_by) REFERENCES players(id)
+  CONSTRAINT fk_teams_created_by FOREIGN KEY (created_by) REFERENCES players(id),
+  CONSTRAINT fk_teams_captain FOREIGN KEY (captain) REFERENCES players(id)
 );
 
 -- Pastime "Franchise" Players e.g. Hitmen Keith Donald, Alexander Weaver, Brian Fisher, etc.
 CREATE TABLE team_players (id bigserial CONSTRAINT pk_team_players PRIMARY KEY,
-  number smallint NOT NULL UNIQUE,
-  nickname varchar(16) UNIQUE,
   team integer NOT NULL,
   player integer NOT NULL,
+  number smallint NOT NULL,
+  nickname varchar(16),
   CONSTRAINT fk_team_players_team FOREIGN KEY (team) REFERENCES teams(id),
   CONSTRAINT fk_team_players_player FOREIGN KEY (player) REFERENCES players(id)  
 );
@@ -47,45 +50,48 @@ CREATE TABLE team_players (id bigserial CONSTRAINT pk_team_players PRIMARY KEY,
 -- Pastime Leagues e.g. South Brevard Adult Flag Football
 CREATE TABLE leagues (id serial CONSTRAINT pk_leagues PRIMARY KEY,
   name varchar(64) NOT NULL,
+  sport varchar(16) NOT NULL,
   created timestamp NOT NULL CONSTRAINT df_leagues_created DEFAULT now()  
 );
 
 -- Pastime League Seasons e.g. South Brevard Adult Flag Football Winter 2012
 CREATE TABLE seasons (id bigserial CONSTRAINT pk_seasons PRIMARY KEY,
-  name varchar(64) NOT NULL,
   league integer NOT NULL,
+  name varchar(64) NOT NULL,
   start_date date,
   status char(1), -- N: new, O: registration open, C: registration closed, R: running, X: over
   created timestamp NOT NULL CONSTRAINT df_seasons_created DEFAULT now(),  
+  league_name varchar(64) NOT NULL,
   CONSTRAINT fk_seasons_league FOREIGN KEY (league) REFERENCES leagues(id)
 );
 
 -- Pastime Team Registrations e.g. Hitmen - South Brevard Adult Flag Football Winter 2012
 CREATE TABLE registered_teams (id bigserial CONSTRAINT pk_registered_teams PRIMARY KEY,
   team integer NOT NULL,
-  league integer NOT NULL,
   season bigint NOT NULL,
+  name varchar(64),
+  captain integer NOT NULL,
   CONSTRAINT fk_registered_teams_team FOREIGN KEY (team) REFERENCES teams(id),
-  CONSTRAINT fk_registered_teams_league FOREIGN KEY (league) REFERENCES leagues(id),  
-  CONSTRAINT fk_registered_teams_season FOREIGN KEY (season) REFERENCES seasons(id)  
+  CONSTRAINT fk_registered_teams_season FOREIGN KEY (season) REFERENCES seasons(id),
+  CONSTRAINT fk_registered_teams_captain FOREIGN KEY (captain) REFERENCES players(id)
 );
 
 -- Pastime Player Registrations e.g. Keith Donald - Hitmen - South Brevard Adult Flag Football Winter 2012
 CREATE TABLE registered_players (id bigserial CONSTRAINT pk_registered_players PRIMARY KEY,
-  team bigint NOT NULL,
+  registered_team bigint NOT NULL,
   player integer NOT NULL,
-  number smallint NOT NULL UNIQUE,
-  nickname varchar(16) UNIQUE,
-  CONSTRAINT fk_registered_players_team FOREIGN KEY (team) REFERENCES registered_teams(id),
+  number smallint NOT NULL,
+  nickname varchar(16),
+  CONSTRAINT fk_registered_players_registered_team FOREIGN KEY (registered_team) REFERENCES registered_teams(id),
   CONSTRAINT fk_registered_players_player FOREIGN KEY (player) REFERENCES players(id)    
 );
 
 -- Pastime Games e.g. Week 1 - Hitmen - South Brevard Adult Flag Football Winter 2012
 CREATE TABLE games (id bigserial CONSTRAINT pk_games PRIMARY KEY,
-  team bigint NOT NULL,
+  registered_team bigint NOT NULL,
   number smallint NOT NULL,  
   opponent bigint NOT NULL,
   start_time timestamp NOT NULL,
-  CONSTRAINT fk_games_team FOREIGN KEY (team) REFERENCES registered_teams(id),
+  CONSTRAINT fk_games_registered_team FOREIGN KEY (registered_team) REFERENCES registered_teams(id),
   CONSTRAINT fk_games_opponent FOREIGN KEY (opponent) REFERENCES registered_teams(id)  
 );
--- src/main/java/com/pastime/next/next-games.sql
+++ src/main/java/com/pastime/next/next-games.sql
@@ -6,21 +6,26 @@ CREATE SCHEMA next;
 GRANT USAGE ON SCHEMA next TO pastime;
 
 -- Pastime Next Games e.g. Hitmen vs Mergers on Feb 22, 2012
-CREATE TABLE next.games (team varchar(16),
+CREATE TABLE next.games (team_slug varchar(16),
   number smallint NOT NULL,
   start_time timestamp NOT NULL,
   opponent varchar(64) NOT NULL,
-  CONSTRAINT pk_games PRIMARY KEY (team, number)  
+  game bigint NOT NULL,
+  CONSTRAINT pk_games PRIMARY KEY (team_slug, number),
+  CONSTRAINT fk_games_game FOREIGN KEY (game) REFERENCES games(id)  
 );
   
 -- Pastime Next Game Attendance e.g. Keith Donald attending Hitmen game 1 this week
-CREATE TABLE next.game_attendance (team varchar(16),
+CREATE TABLE next.game_attendance (team_slug varchar(16),
   game smallint,
-  player varchar(16),  
+  registered_player_slug varchar(16),
   attending boolean,
   update_time timestamp,
-  CONSTRAINT pk_game_attendance PRIMARY KEY (team, game, player),
-  CONSTRAINT fk_game_attendance_team FOREIGN KEY (team, game) REFERENCES next.games(team, number)
+  name varchar(64),  
+  player integer NOT NULL,
+  CONSTRAINT pk_game_attendance PRIMARY KEY (team_slug, game, registered_player_slug),
+  CONSTRAINT fk_game_attendance_game FOREIGN KEY (team_slug, game) REFERENCES next.games(team_slug, number),
+  CONSTRAINT fk_game_attendance_player FOREIGN KEY (player) REFERENCES players(id)  
 );
 
 GRANT USAGE ON ALL SEQUENCES IN SCHEMA next TO pastime;
